e578d89e3f7de050ceeb96407513f3a93cf93c30
lvfangren <m18188267414@163.com>
2022-08-14 19:14:58 +0800

    fix: 修复commitlint没生效问题

--- dashboard/commitlint.config.js
+++ dashboard/commitlint.config.js
@@ -1,11 +1,3 @@
 module.exports = {
-    extends: ['@commitlint/config-conventional'],
-    rules: {
-        'type-enum': [2, 'always', [
-            "feat", "fix", "docs", "style", "refactor", "perf", "test", "build", "ci", "chore", "revert"
-        ]],
-        'subject-full-stop': [0, 'never'],
-        'subject-case': [0, 'never']
-
-    }
+    extends: ['@commitlint/config-conventional']
 };
