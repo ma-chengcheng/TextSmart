c4070533e393d896d6c8797c40699c638c16150c
lvfangren <m18188267414@163.com>
2022-09-13 21:47:59 +0800

    chore: 暂存

--- dashboard/vite.config.ts
+++ dashboard/vite.config.ts
@@ -1,4 +1,5 @@
 import Components from 'unplugin-vue-components/vite';
+import AutoImport from 'unplugin-auto-import/vite';
 import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
 import { resolve } from 'path';
 
@@ -12,12 +13,21 @@ export default defineConfig({
     dirs: ['src/components'],
     deep: true,
     resolvers: [AntDesignVueResolver({ importStyle: false, resolveIcons: true })]
+  }), AutoImport({
+    imports: [
+      'vue',
+      'vue-router',
+    ],
+    dts: 'auto-imports.d.ts',
   })],
   resolve: {
     alias: {
         '@': resolve(__dirname, 'src'),
     },
     extensions: ['*', '.js', '.ts', '.vue', '.json'],
+  },
+  define: {
+    __BUILD_TIME__: new Date(),
   },
   server: {
     cors: true,
